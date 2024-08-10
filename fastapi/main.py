6d7af16c2b59e7a459337023f1325027a7e8f013
Trent Cain <trenton.cain@baptisthealth.net>
2024-08-10 15:32:00 -0500

    Removed the import pickle file from main.py

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 from routers import aiModelRealEstatePredictor
-import pickle
+
 
 app = FastAPI()
 
-model = pickle.load(open("/model/model.pkl", "rb"))
 
 app.include_router(aiModelRealEstatePredictor.router)
