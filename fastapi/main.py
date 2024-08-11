7ccbdf8fdb5fe551ae496735d7fc7249f576b8b5
Trent Cain <trenton.cain@baptisthealth.net>
2024-08-11 03:31:44 -0500

    Added metadata to FastAPI app

--- fastapi/main.py
+++ fastapi/main.py
@@ -3,8 +3,31 @@ from routers import aiModelRealEstatePredictor  # type: ignore
 import pickle
 import asyncio
 
+description = """
+With the Real-Estate House Predictor App, you can locate pinpoint pricing for your next client engagment
+#######
 
-app = FastAPI()
+This is an amazing opportunity to leverage enormous insights over out competitors, and further stablize relationships with
+our customers
+
+With the use of Machine Learning, we can deploy rapid scalibity of our API that, that provides seemless predictions
+"""
+app = FastAPI(title="Real Estate Pricing Predictor ML Model",
+              description=description,
+              summary="There are some humongous houses in the US. Lets determine the price",
+              version="0.0.1",
+              terms_of_service="https://www.phdata.io/legal/website-terms-of-use/",
+              contact={
+                  "name": "phData",
+                  "url": "https://www.phdata.io/about-us/",
+                  "email": "trent.cain@phdata.blah",
+              },
+              license_info={
+                  "name": "OpenAI",
+                  "url": "https://openai.com/",
+              },
+              )
+
 
 model = pickle.load(
     open("/Users/trentcain/ML-Project-phData/model/model.pkl", "rb"))
